de8939884cfbb2f351522b89a3a507875fcdd499
jim-les <jimleston35@gmail.com>
2024-03-29 01:21:23 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -14,5 +14,5 @@ app.use(cors());
 app.use('/api', routes);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
